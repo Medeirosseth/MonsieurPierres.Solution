5d24ef46ee2d0de652bad6e2875f2e05a70f6fa2
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-04 10:44:53 -0700

    add Create to Flavor Controller and View

---
+++ Pierres/Controllers/FlavorsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pierres.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pierres.Controllers
+{
+  public class FlavorsController : Controller 
+  {
+    private readonly PierresContext _db;
+
+    public FlavorsController(PierresContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return ViewModels();
+    }
+  }
+}
