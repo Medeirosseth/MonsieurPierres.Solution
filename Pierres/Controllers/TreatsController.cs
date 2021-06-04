64158d85c1ca62e6e1228c8a68d177cd654c3d1e
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-04 12:21:33 -0700

    add Delete to Treat Controller and Views

--- Pierres/Controllers/TreatsController.cs
+++ Pierres/Controllers/TreatsController.cs
@@ -84,5 +84,47 @@ namespace Pierres.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
       }
+
+      public ActionResult AddFlavor(int id)
+      {
+        var thisFlavor = _db.Flavors.FirstOrDefault(treat => treat.TreatId == id);
+        ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+        return View(thisRecipe);
+      }
+
+      [HttpPost]
+      public ActionResult AddFlavor(Treat treat, int FlavorId)
+      {
+        if (FlavorId != 0)
+        {
+          _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      
+      public ActionResult Delete(int id)
+      {
+        var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+        return ViewModels(thisTreat);
+      }
+
+      [HttpPost, ActionName("Delete")]
+      public ActionResult DeleteConfrimed(int id)
+      {
+        var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+        _db.Treats.Remove(thisTreat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      [HttpPost]
+      public ActionResult DeleteFlavor(int joinId)
+      {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      }
     }
 }
