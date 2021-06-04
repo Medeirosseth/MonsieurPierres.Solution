1887021e5ad3b2e4944961272d8c6ba4fd8cebeb
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-04 11:52:38 -0700

    add Details to Treats Controller and View

--- Pierres/Controllers/TreatsController.cs
+++ Pierres/Controllers/TreatsController.cs
@@ -46,5 +46,14 @@ namespace Pierres.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
       }
+
+      public ActionResult Details(int id)
+      {
+        var thisTreat = _db.Treats
+        .Include(treat => treat.JoinEntities)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId ==id);
+        return ViewModels(thisTreat);
+      }
     }
 }
